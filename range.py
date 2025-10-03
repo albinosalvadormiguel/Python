90f452542c17588238fc2594b80043b75c47d936
albinosalvadormiguel <proyectoyunke@gmail.com>
2025-10-03 15:50:35 +0200

    range

---
+++ range.py
@@ -0,0 +1,14 @@
+import os
+os.system("cls")
+
+nums = range(10)
+list_of_nums = list(nums)
+print(list_of_nums)
+
+
+contador = 0
+while contador < 5:
+  print("Hacer cinco veces algo")
+  contador += 1 
+for _ in range(5):
+    print("Hacer cinco veces algo")
